e417e338db87adee460d81faec6ffb7aa9e98020
subir701 <subiradhikari181@gmail.com>
2026-04-03 13:07:58 +0530

    Project Structure

---
+++ Finsecure/src/main/java/com/finSecure/FinsecureApplication.java
@@ -0,0 +1,13 @@
+package com.finSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinsecureApplication.class, args);
+	}
+
+}
---
+++ Finsecure/src/test/java/com/finSecure/FinsecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinsecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
